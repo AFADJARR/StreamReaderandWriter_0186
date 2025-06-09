f50893f7f22846cf1177ea4ce893c7fc80d48656
AFADJARR <achmad.fadjar.ft24@mail.umy.ac.d>
2025-06-09 15:33:26 +0700

    uji exception handling saat mengakses elemen array di luar indeks

---
+++ ExceptionStandart.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <exception>
+//untuk objek exception yang akan digunakan
+#include <array>
+// untuk ibjek array yang digunakan
+using namespace std;
+
+int main(){
+
+    cout << "Awal Program" << endl; // penanda 1:...
+    try {
+        array<int, 3> data = ( 10, 20, 30 );
+        //pesan array integer 3 elemen
+        cout << data.at(5) << endl;
+        //memanggil array elemen ke 5
+    }
+}
